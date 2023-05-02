18fbf172ca5bf480d77b2a54fff4f115fed84d65
Olitera <lu_gu@mail.ru>
2023-05-02 11:35:21 +0200

    fix: fix checkClick function and CapsLook

--- index.js
+++ index.js
@@ -10,12 +10,24 @@ mainClass.getData().then((data) => {
   const container = mainClass.createContainer();
   const keys = data.map((element, i) => {
     const key = new Key(element).createKey(isEn);
-    key.addEventListener('click', () => mainClass.checkClick(textarea, element.value, i));
+    key.addEventListener('click', () => {
+      const value = keys[i].innerText;
+      return mainClass.checkClick(textarea, value, i);
+    });
     container.append(key);
     return key;
   });
   mainClass.buttonsElements = keys;
   const text = mainClass.createText();
+
+  if (mainClass.isCapslook) {
+    for (let i = 0; i < keys.length; i++) {
+      if (keys[i].innerText.match(/\D/) && keys[i].innerText.length === 1 && data[i].value.length === 1) {
+        keys[i].innerText = keys[i].innerText.toUpperCase();
+      }
+    }
+    keys[29].classList.add('active');
+  }
 
   // mainClass.activateMouseEnter(textarea);
 
@@ -28,6 +40,13 @@ mainClass.getData().then((data) => {
     }
     if (event.code === 'CapsLock') {
       mainClass.isCapslook = true;
+      localStorage.setItem('isCapsLock', 'true');
+      event.preventDefault();
+      for (let i = 0; i < keys.length; i++) {
+        if (keys[i].innerText.match(/\D/) && keys[i].innerText.length === 1 && data[i].value.length === 1) {
+          keys[i].innerText = keys[i].innerText.toUpperCase();
+        }
+      }
     }
     if (event.keyCode == 16) {
       if (isEn) {
@@ -61,6 +80,9 @@ mainClass.getData().then((data) => {
             if (data[i].value3) {
               container.children[i].innerText = data[i].value3;
             }
+            if (keys[i].innerText.match(/\D/) && keys[i].innerText.length === 1 && data[i].value.length === 1) {
+              keys[i].innerText = mainClass.isCapslook ? keys[i].innerText.toUpperCase() : keys[i].innerText.toLowerCase();
+            }
             // if(data[i].value.match(/[a-zA-Z]/) && data[i].value.length === 1) {
             //   // console.log(data[i].value);
             //   container.children[i].innerText = data[i].value.toUpperCase();
@@ -72,6 +94,9 @@ mainClass.getData().then((data) => {
           for (let i = 0; i < container.children.length; i++) {
             if (data[i].value && data[i].value !== '&#8896' && data[i].value !== '&#8897') {
               container.children[i].innerText = data[i].value;
+            }
+            if (keys[i].innerText.match(/\D/) && keys[i].innerText.length === 1 && data[i].value.length === 1) {
+              keys[i].innerText = mainClass.isCapslook ? keys[i].innerText.toUpperCase() : keys[i].innerText.toLowerCase();
             }
             // if(data[i].value.match(/[a-zA-Z]/) && data[i].value.length === 1) {
             //   // console.log(data[i].value);
@@ -87,6 +112,17 @@ mainClass.getData().then((data) => {
     // console.log(event.code);
     textarea.focus();
     const index = data.findIndex(({ code }) => code === event.code);
+
+    console.log(event.keyCode);
+    if (event.keyCode != 32 && event.keyCode != 9 && event.keyCode != 20 && event.keyCode != 16 && event.keyCode != 17 && event.keyCode != 18 && event.keyCode != 13 && event.keyCode != 37 && event.keyCode != 38 && event.keyCode != 39 && event.keyCode != 40 && event.keyCode != 91) {
+      event.preventDefault();
+      const start = textarea.selectionStart;
+      if (keys[index]) {
+        textarea.value = mainClass.addSymbol(textarea.value, textarea.selectionStart, keys[index].innerText);
+      }
+      textarea.selectionEnd = start + 1;
+    }
+
     if (index && keys[index]) {
       keys[index].classList.add('active');
     }
@@ -99,6 +135,7 @@ mainClass.getData().then((data) => {
   function onKeyUp(event) {
     if (event.code === 'CapsLock') {
       mainClass.isCapslook = false;
+      localStorage.setItem('isCapsLock', 'false');
     }
     const index = data.findIndex(({ code }) => code === event.code);
     if (index && keys[index]) {
@@ -132,6 +169,14 @@ mainClass.getData().then((data) => {
     }
 
     if (event.keyCode == 18) {
+    }
+
+    if (event.keyCode == 20) {
+      for (let i = 0; i < keys.length; i++) {
+        if (keys[i].innerText.match(/\D/) && keys[i].innerText.length === 1 && data[i].value.length === 1) {
+          keys[i].innerText = keys[i].innerText.toLowerCase();
+        }
+      }
     }
   }
 
